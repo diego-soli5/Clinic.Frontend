ea7a63257a10ecbe87d50352ac765e91993ea749
Luis Diego Solis Camacho <1diego321@gmail.com>
2021-06-29 10:33:14 -0600

    Agregar archivos de proyecto.

---
+++ Clinic.Data/Abstractions/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Data.Abstractions
+{
+    public interface IRepository
+    {
+    }
+}
---
+++ Clinic.Web/Areas/Admin/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
