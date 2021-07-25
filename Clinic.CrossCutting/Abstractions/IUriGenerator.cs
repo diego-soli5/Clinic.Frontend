ae02af3b35ef646e105bc346e2590a92f05458f3
Luis Diego Solis Camacho <1diego321@gmail.com>
2021-07-25 16:43:21 -0600

    Documentacion XML UriGenerator

--- Clinic.CrossCutting/Abstractions/IUriGenerator.cs
+++ Clinic.CrossCutting/Abstractions/IUriGenerator.cs
@@ -5,8 +5,22 @@ namespace Clinic.CrossCutting.Abstractions
 {
     public interface IUriGenerator
     {
+        /// <summary>
+        /// Añade parametros queryString necesarios para la paginación y los pasados por parametro a la URL base.
+        /// </summary>
+        /// <typeparam name="TQueryFilter">El tipo de parametros de filtros y paginación</typeparam>
+        /// <param name="baseUri">URL base para añadir los queryString.</param>
+        /// <param name="filter">Los filtros de paginación y consulta.</param>
+        /// <returns></returns>
         Uri CreatePagedListUri<TQueryFilter>(string baseUri, TQueryFilter filter)
              where TQueryFilter : BaseQueryFilter;
+        /// <summary>
+        /// Añade parametros queryString pasados por parametro a la URL base.
+        /// </summary>
+        /// <typeparam name="TQueryFilter">El tipo de parametros de filtros y paginación</typeparam>
+        /// <param name="baseUri">URL base para añadir los queryString.</param>
+        /// <param name="filter">Los filtros de paginación y consulta.</param>
+        /// <returns></returns>
         Uri AddQueryStringParams(string baseUri, object queryStringParams);
     }
 }
--- Clinic.Domain/Filters/IsAlreadyAuthenticatedFilter.cs
+++ Clinic.Domain/Filters/IsAlreadyAuthenticatedFilter.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using System.Threading.Tasks;
 
 namespace Clinic.Domain.Filters
 {
